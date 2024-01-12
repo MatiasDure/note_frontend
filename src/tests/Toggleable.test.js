21e7f5634271928eb573cfb208f46b281cfb9d3f
Matias Dure <497232@student.saxion.nl>
2024-01-12 21:35:15 +0100

    created tests for note and toggleable components

---
+++ src/tests/Toggleable.test.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Toggleable from "../components/Toggleable";
